da67dc4102584bfd3035cf032f2bcb7053e5e67a
Aisha Co <aishaysabel02@gmail.com>
2021-12-03 20:41:28 -0800

    moved initial chatbot code to different folder

---
+++ chatbot_attempts/chatbot/src/chatbot/ActionProvider.js
@@ -0,0 +1,35 @@
+// ActionProvider starter code
+class ActionProvider {
+    constructor(
+     createChatBotMessage,
+     setStateFunc,
+     createClientMessage,
+     stateRef,
+     createCustomMessage,
+     ...rest
+   ) 
+   
+   {
+     this.createChatBotMessage = createChatBotMessage;
+     this.setState = setStateFunc;
+     this.createClientMessage = createClientMessage;
+     this.stateRef = stateRef;
+     this.createCustomMessage = createCustomMessage;
+   }
+
+   greet = () =>
+   {
+      const message = this.createChatBotMessage("Hello friend.");
+      this.addMessageToState(message);
+   }
+
+   addMessageToState = (message) =>
+   {
+      this.setState(prevState => ({
+        ...prevState,
+        messages: [...prevState.messages, message],
+      }))
+   }
+ }
+ 
+ export default ActionProvider;
---
+++ chatbot_attempts/chatbot/src/chatbot/MessageParser.js
@@ -0,0 +1,19 @@
+// MessageParser starter code
+class MessageParser {
+    constructor(actionProvider, state) {
+      this.actionProvider = actionProvider;
+      this.state = state;
+    }
+  
+    parse(message) {
+      console.log(message);
+      const lowercase = message.toLowerCase();
+
+      if(lowercase.includes("hello"))
+      {
+        this.actionProvider.greet();
+      }
+    }
+  }
+  
+  export default MessageParser;
