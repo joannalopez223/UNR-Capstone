b1b1e727d12b81b0c6c04eb843da50fa0a078f3d
Joanna Lopez <79895381+joannalopez223@users.noreply.github.com>
2021-12-05 17:30:40 -0800

    Adding postwidget

--- src/components/Widget/PostWidget/PostWidget.js
+++ src/components/Widget/PostWidget/PostWidget.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import styles from '../PostWidget/PostWidget.module.css'
 
+function PostWidget(props){
+    return (
+        <div className= {styles.icon}>
+            <span className="material-icons">account_circle</span>
+        </div>
+    );
+};
+
+export default PostWidget;
