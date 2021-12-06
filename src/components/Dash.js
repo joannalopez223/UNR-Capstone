e3393d13801008be4a3c30575e61e7c2cd1b0b9e
Joanna Lopez <79895381+joannalopez223@users.noreply.github.com>
2021-12-05 17:28:09 -0800

    Adding Dashboard files
    Dashboard calling active message and active request js files

---
+++ src/components/Dash.js
@@ -0,0 +1,22 @@
+import styles from './Dash.module.css';
+import React from 'react';
+import ActiveMessage from './ActiveMessage/ActiveMessage';
+import ActiveRequest from './ActiveRequest/ActiveRequest';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+
+function Dash() {
+    return (
+        <div className= {styles.dashboard}>
+        <div className={styles.left_pane}>
+            <ActiveRequest />
+            <ActiveMessage />
+        </div>
+        <div className={styles.right_pane}>
+            <Calendar />
+        </div>
+    </div>
+);
+}
+
+export default Dash;
