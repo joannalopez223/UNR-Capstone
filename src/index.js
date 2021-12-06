62ed9ad6dafa6c203f63c41926df14eee010ddfe
Joanna Lopez <79895381+joannalopez223@users.noreply.github.com>
2021-12-05 17:37:01 -0800

    adding aws to backend

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+
+import Amplify from "aws-amplify";
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
+   </BrowserRouter>,
   document.getElementById('root')
 );
-
