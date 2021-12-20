3417eebeedc0ac0008fd2867281d6ef6e1e13e92
joannalopez223 <joannalopez@nevada.unr.edu>
2021-12-20 13:21:33 -0800

    Feedback page

---
+++ src/pages/Feedback/Feedback.js
@@ -0,0 +1,12 @@
+import React from "react";
+import FeedbackRequest from "../../components/Feedback/FeedbackRequest";
+
+const Feedback = () => {
+  return (
+    <div className="app">
+      <FeedbackRequest />
+    </div>
+  );
+};
+
+export default Feedback;
